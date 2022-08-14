130084122d0c249a75139b8ffc855488e0a44ec4
xxx <humengmingxx@gmail.com>
2022-08-14 23:22:10 +0800

    chore：build conf

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/extension.ts'],
-  format: ['cjs'],
+  format: ['cjs', 'esm'],
   shims: false,
   dts: false,
   external: ['vscode'],

25c0d6dd549979909b59ad53f5e8746a45c65fd4
chai2010 <chaishushan@gmail.com>
2023-05-11 08:50:59 +0800

    zz

---
+++ dev.md
@@ -0,0 +1,13 @@
+# 本地开发测试
+
+```
+$ cnpm install -g yo generator-code
+$ cnpm install
+```
+
+## 参考资料
+
+- https://code.visualstudio.com/api/get-started/your-first-extension
+- https://github.com/microsoft/vscode-extension-samples/tree/main/helloworld-sample
+- https://code.visualstudio.com/api/language-extensions/overview
+- https://github.com/microsoft/vscode-extension-samples/tree/main/lsp-sample
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,19 @@
-import { commands, workspace } from 'vscode'
-import type { ExtensionContext } from 'vscode'
+import * as vscode from 'vscode';
+
 import { runWaCode } from './run'
 import { fmtWaCode } from './fmt'
 
-export function activate(context: ExtensionContext) {
-  context.subscriptions.push(commands.registerCommand('wa.runWaCode', runWaCode))
-  context.subscriptions.push(workspace.onDidSaveTextDocument(fmtWaCode))
+export function activate(context: vscode.ExtensionContext) {
+	console.log("wa extensio actived!");
+
+	context.subscriptions.push(vscode.commands.registerCommand('wa.runWaCode', runWaCode));
+	context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(fmtWaCode));
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("wa.helloWorld", () => {
+			vscode.window.showInformationMessage('Wa: Hello World!');
+		})
+	);
 }
 
 export function deactivate() { }
